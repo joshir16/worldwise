9de62c35d9a59beb0bf23d36b634d80116617660
Rishabh <joshir838@gmail.com>
2025-03-13 11:00:58 +0530

    nested Routes

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={styles.nav}>App navigation</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <NavLink to="cities">Cities</NavLink>
+        <NavLink to="countries">Countries</NavLink>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
